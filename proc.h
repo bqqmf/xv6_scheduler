34130f214511b53a11ea6dbb3921a0f951fcaf55
ojh <wjdghks728@naver.com>
2024-08-21 06:22:01 +0000

    Add extra variables in struct proc

--- proc.h
+++ proc.h
@@ -32,11 +32,11 @@ struct context {
   uint eip;
 };
 
-enum procstate = UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-= uint sz;                     // Size of process memory (bytes)
+  uint sz;                     // Size of process memory (bytes)
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
