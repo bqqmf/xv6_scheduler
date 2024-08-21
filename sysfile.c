6e2c7402f6e77858a0e0c68858e05499275c4708
ojh <wjdghks728@naver.com>
2024-08-21 06:45:26 +0000

    Increase r/w proc's rw_cnt

--- sysfile.c
+++ sysfile.c
@@ -75,6 +75,7 @@ sys_read(void)
 
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
+  if (myproc()) myproc()->rw_cnt ++;
   return fileread(f, p, n);
 }
 
@@ -87,6 +88,7 @@ sys_write(void)
 
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
+  if (myproc()) myproc()->rw_cnt ++;
   return filewrite(f, p, n);
 }
 
