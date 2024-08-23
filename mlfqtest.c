9a0d28ac661873f8ad59a239c9de2b204a167517
ojh <wjdghks728@naver.com>
2024-08-23 08:05:52 +0000

    Modify testcase

--- mlfqtest.c
+++ mlfqtest.c
@@ -3,16 +3,16 @@
 #include "user.h"
 #include "fcntl.h"
 
-#define NPROC 12 
-#define INPUT_FILENAME "README"
+#define N 12 
+#define INPUT_FILENAME "sh"
 #define OUTPUT_FILENAME1 "io_test1.txt"
 #define OUTPUT_FILENAME2 "io_test2.txt"
 #define BUFFER_SIZE 1024
 
-void cpu_bound_task(int n) {
+void cpu_bound_task() {
     int i;
     volatile int sum = 0;
-    for (i=0; i<30000000; i++)
+    for (i=0; i<200000000; i++)
         sum += i;
 
 }
@@ -74,35 +74,44 @@ void io_bound_task(int T, const char *input, const char *output) {
 
 int main(void) {
     int pid;
+    /*
     int i;
 
-    for (i = 0; i < NPROC; i++) {
+    for (i = 0; i < N; i++) {
         pid = fork();
         if (pid == 0) { // 자식 프로세스
-                        //printf(1, "\nProcess %d created.\n", i);
+                        //printf(1, "\Ness %d created.\n", i);
             if (i % 3 == 0) {
                 printf(1, "CPU-bound Process %d created\n", i);
-                cpu_bound_task(100 * (i + 1));  // CPU-bound 작업
+                cpu_bound_task());  // CPU-bound 작업
                 printf(1, "CPU-bound Process %d finished\n", i);
                 exit();
-            } else if (i % 3 == 2) {  
+            } else if (i % 3 == 1) {  
+                printf(1, "CPU + IO Process %d created\n", i);
+                cpu_bound_task();  
+                io_bound_task(1, INPUT_FILENAME, OUTPUT_FILENAME1);
+                printf(1, "CPU + IO Process %d finished\n", i);
+                exit();
+            } else {
                 printf(1, "IO-bound Process %d created\n", i);
                 io_bound_task(1, INPUT_FILENAME, OUTPUT_FILENAME2);
                 printf(1, "IO-bound Process %d finished\n", i);
-                exit();
-            } else {
-                printf(1, "CPU + IO Process %d created\n", i);
-                cpu_bound_task(100);  
-                io_bound_task(1, INPUT_FILENAME, OUTPUT_FILENAME1);
-                printf(1, "CPU + IO Process %d finished\n", i);
                 exit();
             }
         }
     }
 
-    for (i = 0; i < NPROC; i++) {
+    for (i = 0; i < N; i++) {
         wait();
     }
+    */
+    pid = fork();
+    if (pid == 0) {
+        cpu_bound_task(); // 4.5
+        //io_bound_task(5, INPUT_FILENAME, OUTPUT_FILENAME2); //9~10
+        exit();
+    }
+    wait();
 
     printf(1, "All processes finished\n");
     exit();
